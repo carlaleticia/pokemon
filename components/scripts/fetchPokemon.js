4f25ec480b30ad8210ed4b1454c3a3fc5eab9653
Carla <carlaleticia.gf@gmail.com>
2022-07-27 15:43:03 -0300

    add footer

--- components/scripts/fetchPokemon.js
+++ components/scripts/fetchPokemon.js
@@ -49,6 +49,11 @@ const createPokemonCard = (pokemon) => {
 	const pokeId = pokemon.id.toString().padStart(3, '0');
 
 	const pokeAllTypes = pokemon.types.map((type) => type.type.name);
+	const types = pokeAllTypes.map((type) => {
+		// Study this part
+		return `<ul class="pokeType">${type}</ul>`;
+	});
+
 	const pokeType = pokeMainTypeColor.find((type) => pokeAllTypes.indexOf(type) > -1); //  get the bgcolor for the card
 	const pokeColor = pokeColors[pokeType];
 
@@ -57,7 +62,7 @@ const createPokemonCard = (pokemon) => {
 	const pokemonInnerHTML = `
     <div class="info">
         <h4 class="pokeName">${pokeName}</h4>
-        <ul class="pokeType">${pokeAllTypes}</ul>
+        ${types}
 
     </div>
     <div class="pokeImg">
@@ -74,90 +79,87 @@ const createPokemonCard = (pokemon) => {
 	// pokeCard.addEventListener('click', createPokemonFullCard());
 
 	// Create Full Card Pokemon
-	const createPokemonFullCard = (pokemon) => {
-		let pokeDescription = document.getElementsByClassName('card');
-		let pokeCardDesc = document.createElement('div');
-		pokeCardDesc.classList.add('pokeCardContainer');
+};
 
-		// pokeDescription.addEventListener('click', (createPokemonFullCard.innerHTML = displayInfoInnerHTML));
+const createPokemonFullCard = (pokemon) => {
+	const pokeAbilities = pokemon.abilities.map((abilities) => abilities.ability.name.replace('-', ' '));
+	const pokeExperience = pokemon.base_experience;
+	const pokeHealth = pokemon.stats[0].base_stat;
+	const pokeAttack = pokemon.stats[1].base_stat;
+	const pokeDefense = pokemon.stats[2].base_stat;
+	const pokeAttackSuper = pokemon.stats[3].base_stat;
+	const pokeDefenseSuper = pokemon.stats[4].base_stat;
 
-		const pokeAbilities = pokemon.abilities.map((abilities) => abilities.ability.name.replace('-', ' '));
-		const pokeExperience = pokemon.base_experience;
-		const pokeHealth = pokemon.stats[0].base_stat;
-		const pokeAttack = pokemon.stats[1].base_stat;
-		const pokeDefense = pokemon.stats[2].base_stat;
-		const pokeAttackSuper = pokemon.stats[3].base_stat;
-		const pokeDefenseSuper = pokemon.stats[4].base_stat;
+	function getPokeGeneretion() {
+		if (pokemon.id > 0 && pokemon.id <= 151) return 'Generation 1';
+		if (pokemon.id > 151 && pokemon.id <= 251) return 'Generation 2';
+		if (pokemon.id > 251 && pokemon.id <= 386) return 'Generation 3';
+		if (pokemon.id > 386 && pokemon.id <= 493) return 'Generation 4';
+		if (pokemon.id > 493 && pokemon.id <= 649) return 'Generation 5';
+		if (pokemon.id > 649 && pokemon.id <= 721) return 'Generation 6';
+		if (pokemon.id > 721 && pokemon.id <= 809) return 'Generation 7';
+		if (pokemon.id > 809 && pokemon.id <= 890) return 'Generation 8';
+		if (pokemon.id <= 0 || pokemon.id > 891) return 'New';
+	}
 
-		function getPokeGeneretion() {
-			if (pokemon.id > 0 && pokemon.id <= 151) return 'Generation 1';
-			if (pokemon.id > 151 && pokemon.id <= 251) return 'Generation 2';
-			if (pokemon.id > 251 && pokemon.id <= 386) return 'Generation 3';
-			if (pokemon.id > 386 && pokemon.id <= 493) return 'Generation 4';
-			if (pokemon.id > 493 && pokemon.id <= 649) return 'Generation 5';
-			if (pokemon.id > 649 && pokemon.id <= 721) return 'Generation 6';
-			if (pokemon.id > 721 && pokemon.id <= 809) return 'Generation 7';
-			if (pokemon.id > 809 && pokemon.id <= 890) return 'Generation 8';
-			if (pokemon.id <= 0 || pokemon.id > 891) return 'New';
-		}
+	const displayInfoInnerHTML = `
+	<main class="displayCard">
+	<container class="pokeCard">
+		<div class="pokeImgCard">
+			<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg" alt="${pokeName}">
+			<ul class="pokeType">${pokeAllTypes}</ul>
+		</div>
 
-		const displayInfoInnerHTML = `
-		<main class="displayCard">
-		<container class="pokeCard">
-			<div class="pokeImgCard">
-				<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg" alt="${pokeName}">
-				<ul class="pokeType">${pokeAllTypes}</ul>
+		<div class="pokeInfoCard">
+			<div class="pokeInfo">
+				<div class="pokeTitle">
+					<h3 class="pokeName">${pokeName}</h3>
+					<p class="pokeGen">${getPokeGeneretion()}</p>
+				</div>
+				<p class="pokeId">#${pokeId}</p>
 			</div>
-
-			<div class="pokeInfoCard">
-				<div class="pokeInfo">
-					<div class="pokeTitle">
-						<h3 class="pokeName">${pokeName}</h3>
-						<p class="pokeGen">${getPokeGeneretion()}</p>
-					</div>
-					<p class="pokeId">#${pokeId}</p>
+			<div class="pokeAbilities">
+				<h3>Abilities</h3>
+				<p> ${pokeAbilities}</p>
+			</div>
+			<div class="points">
+				<div class="pokeExperience">
+					<h3>Experience</h3>
+					<p>${pokeExperience}</p>
 				</div>
-				<div class="pokeAbilities">
-					<h3>Abilities</h3>
-					<p> ${pokeAbilities}</p>
+				<div class="pokeHealth">
+					<h3>Healthy Points</h3>
+					<p>${pokeHealth}</p>
+				</div> 
+			</div>
+			<div class="pokeRangers">
+				<div>
+					<p class="pokeAttack">${pokeAttack}</p>
+					<p>Attack</p>
 				</div>
-				<div class="points">
-					<div class="pokeExperience">
-						<h3>Experience</h3>
-						<p>${pokeExperience}</p>
-					</div>
-					<div class="pokeHealth">
-						<h3>Healthy Points</h3>
-						<p>${pokeHealth}</p>
-					</div> 
+				<div>
+					<p class="pokeDefense">${pokeDefense}</p>
+					<p>Defense</p>
+					
 				</div>
-				<div class="pokeRangers">
-					<div>
-						<p class="pokeAttack">${pokeAttack}</p>
-						<p>Attack</p>
-					</div>
-					<div>
-						<p class="pokeDefense">${pokeDefense}</p>
-						<p>Defense</p>
-						
-					</div>
-					<div >
-						<p class="pokeAttackSuper">${pokeAttackSuper}</p>
-						<p>Sp Attack</p>
-					</div>
-					<div >
-						<p class="pokeDefense">${pokeDefenseSuper}</p>
-						<p>Sp Defense</p>
-					</div>
+				<div >
+					<p class="pokeAttackSuper">${pokeAttackSuper}</p>
+					<p>Sp Attack</p>
+				</div>
+				<div >
+					<p class="pokeDefense">${pokeDefenseSuper}</p>
+					<p>Sp Defense</p>
 				</div>
 			</div>
-		</container>
-		<main>
-		`;
+		</div>
+	</container>
+	<main>
+	`;
 
-		pokeCardDesc.innerHTML = displayInfoInnerHTML;
-		pokeDescription.appendChild(pokeCardDesc);
-	};
+	pokeCardDesc.innerHTML = displayInfoInnerHTML;
+	pokeDescription.appendChild(pokeCardDesc);
 };
 
 fetchPokemons();
+const card = document.querySelector.getElementsByClassName('card');
+card.addEventListener('click', (createPokemonFullCard.innerHTML = displayInfoInnerHTML));
